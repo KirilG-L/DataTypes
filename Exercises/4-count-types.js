a178ba6c07c7d4b6cfe2142cad01f4fca4acf91b
Kiril <kiril.gudim@gmail.com>
2019-12-09 23:48:11 +0200

    Lab 2, exercise 4 complete

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,16 +1,14 @@
 'use strict';
 
 const countTypesInArray = arr => {
-  const types = {
-    number: 0,
-    string: 0,
-    boolean: 0,
-    object: 0,
-  };
+  const types = {};
   for (const key of arr) {
     const type = typeof key;
-    types[type] += 1;
+    types[type] ?
+      types[type] += 1 :
+      types[type] = 1;
   }
+  return types;
 };
 const arr = [true, 'hello', 5, 12, -200, false, false, 'word', 'panda', {}, ];
 countTypesInArray(arr);
