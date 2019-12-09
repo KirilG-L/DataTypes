5f4c26b24d07be9a18e347bb755962379ab8b711
Kiril <kiril.gudim@gmail.com>
2019-12-09 22:48:18 +0200

    Lab 2, exercise 4(before keys removal

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,12 +1,17 @@
 'use strict';
 
-const countTypesInArray = ;
-const arr = [true, 'hello', 5, 12, -200, false, false, 'word', 2.22, 'panda', {}, ];
-const types = {
-  number: 0,
-  float: 0,
-  string: 0,
-  boolean: 0,
-  object: 0,
+const countTypesInArray = arr => {
+  const types = {
+    number: 0,
+    string: 0,
+    boolean: 0,
+    object: 0,
+  };
+  for (const key of arr) {
+    const type = typeof key;
+    types[type] += 1;
+  }
 };
+const arr = [true, 'hello', 5, 12, -200, false, false, 'word', 'panda', {}, ];
+countTypesInArray(arr);
 module.exports = { countTypesInArray };
